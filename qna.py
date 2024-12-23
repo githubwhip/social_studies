8289e7641825bfc8f79c4b05a5051983e355ffb5
githubwhip <y0unxxdme@gmail.com>
2024-12-23 14:00:26 +0900

    Create qna.py

---
+++ qna.py
@@ -0,0 +1,156 @@
+import streamlit as st
+import tiktoken
+from loguru import logger
+
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import Docx2txtLoader
+from langchain.document_loaders import UnstructuredPowerPointLoader
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import HuggingFaceEmbeddings
+
+from langchain.memory import ConversationBufferMemory
+from langchain.vectorstores import FAISS
+
+# from streamlit_chat import message
+from langchain.callbacks import get_openai_callback
+from langchain.memory import StreamlitChatMessageHistory
+
+# 내장된 OpenAI API 키
+OPENAI_API_KEY = "your_openai_api_key_here"
+
+def main():
+    st.set_page_config(
+        page_title="사회공부를 해보자",
+        page_icon=":books:"
+    )
+
+    st.image("qna.png")
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
+    if "processComplete" not in st.session_state:
+        st.session_state.processComplete = None
+
+    with st.sidebar:
+        uploaded_files = st.file_uploader("Upload your file", type=['pdf', 'docx'], accept_multiple_files=True)
+        process = st.button("Process")
+
+    if process:
+        if not OPENAI_API_KEY:
+            st.info("API 키가 설정되지 않았습니다. 개발자에게 문의하세요.")
+            st.stop()
+        files_text = get_text(uploaded_files)
+        text_chunks = get_text_chunks(files_text)
+        vetorestore = get_vectorstore(text_chunks)
+
+        st.session_state.conversation = get_conversation_chain(vetorestore, OPENAI_API_KEY)
+        st.session_state.processComplete = True
+
+    if 'messages' not in st.session_state:
+        st.session_state['messages'] = [{"role": "assistant", 
+                                         "content": """\
+🚗✨ 안녕하세요! 사회 인물 조사 학습페이지에 오신 것을 환영합니다! ✨🔋
+
+오른쪽 메뉴바에서 사회 인물 관련 pdf 파일을 업로드해보세요! 📄
+그리고 process버튼을 누르면 똑똑한 챗봇과 대화를 시작할 수 있어요. 🤖💬
+
+궁금한 점이 있거나 심화된 정보가 필요하다면, 챗봇이 척척 도와드릴 거예요! 🧐🚘
+"역사 인물의 모든 것, 궁금할 땐 여기를 톡톡!" 🚀"""}]
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    history = StreamlitChatMessageHistory(key="chat_messages")
+
+    # Chat logic
+    if query := st.chat_input("질문을 입력해주세요."):
+        st.session_state.messages.append({"role": "user", "content": query})
+
+        with st.chat_message("user"):
+            st.markdown(query)
+
+        with st.chat_message("assistant"):
+            chain = st.session_state.conversation
+
+            with st.spinner("Thinking..."):
+                result = chain({"question": query})
+                with get_openai_callback() as cb:
+                    st.session_state.chat_history = result['chat_history']
+                response = result['answer']
+                source_documents = result['source_documents']
+
+                st.markdown(response)
+                with st.expander("참고 문서 확인"):
+                    for doc in source_documents:
+                        st.markdown(doc.metadata['source'], help=doc.page_content)
+
+        # Add assistant message to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
+def tiktoken_len(text):
+    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokens = tokenizer.encode(text)
+    return len(tokens)
+
+def get_text(docs):
+    doc_list = []
+    for doc in docs:
+        file_name = doc.name  # doc 객체의 이름을 파일 이름으로 사용
+        with open(file_name, "wb") as file:  # 파일을 doc.name으로 저장
+            file.write(doc.getvalue())
+            logger.info(f"Uploaded {file_name}")
+        if '.pdf' in doc.name:
+            loader = PyPDFLoader(file_name)
+            documents = loader.load_and_split()
+        elif '.docx' in doc.name:
+            loader = Docx2txtLoader(file_name)
+            documents = loader.load_and_split()
+        elif '.pptx' in doc.name:
+            loader = UnstructuredPowerPointLoader(file_name)
+            documents = loader.load_and_split()
+
+        doc_list.extend(documents)
+    return doc_list
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=900,
+        chunk_overlap=100,
+        length_function=tiktoken_len
+    )
+    chunks = text_splitter.split_documents(text)
+    return chunks
+
+def get_vectorstore(text_chunks):
+    embeddings = HuggingFaceEmbeddings(
+        model_name="jhgan/ko-sroberta-multitask",
+        model_kwargs={'device': 'cpu'},
+        encode_kwargs={'normalize_embeddings': True}
+    )
+    vectordb = FAISS.from_documents(text_chunks, embeddings)
+    return vectordb
+
+def get_conversation_chain(vetorestore, openai_api_key):
+    llm = ChatOpenAI(openai_api_key=openai_api_key, model_name='gpt-3.5-turbo', temperature=0)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        chain_type="stuff",
+        retriever=vetorestore.as_retriever(search_type='mmr', vervose=True),
+        memory=ConversationBufferMemory(memory_key='chat_history', return_messages=True, output_key='answer'),
+        get_chat_history=lambda h: h,
+        return_source_documents=True,
+        verbose=True
+    )
+    return conversation_chain
+
+if __name__ == '__main__':
+    main()
